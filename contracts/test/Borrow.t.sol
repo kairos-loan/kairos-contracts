2cd784b0385247f65c5ee1f3963cec67297ba9d9
npasquie <nicpas@live.fr>
2022-08-10 10:26:06 +0200

    ✅  test wrong nft address

--- contracts/test/Borrow.t.sol
+++ contracts/test/Borrow.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.15;
 import "./SetUp.sol";
 
 contract BorrowTest is SetUp {
+    // struct myCustomOffer
+
     function testSimpleBorrow() public {
         bytes memory emptyBytes;
         bytes memory data = abi.encode(Borrow.BorrowArgs({
@@ -23,4 +25,31 @@ contract BorrowTest is SetUp {
 
         nft.safeTransferFrom(address(this), address(nftaclp), 1, data);
     }
+
+    function testWrongNFTAddress() public {
+        bytes memory emptyBytes;
+        bytes memory data = abi.encode(Borrow.BorrowArgs({
+            offer: Offer({
+                assetToLend: money,
+                loanToValue: 1 ether,
+                duration: 2 weeks,
+                nonce: 0,
+                collatSpecType: CollatSpecType.Floor,
+                tranche: 0,
+                collatSpecs: abi.encode(FloorSpec({
+                    collateral: IERC721(address(1))
+                }))
+            }),
+            signature: emptyBytes
+        }));
+
+        vm.expectRevert(abi.encodeWithSelector(
+            NFTContractDoesntMatchOfferSpecs.selector,
+            nft,
+            IERC721(address(1))
+        ));
+        nft.safeTransferFrom(address(this), address(nftaclp), 1, data);
+    }
+
+    // todo : test unknown collat spec type
 }
