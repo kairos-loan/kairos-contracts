14ceb3175fd8a373c01d6bb4d39fb4e49ac05581
adriankinsey <oojams@live.fr>
2022-11-29 13:00:53 +0100

    Test Error : - LoanAlreadyRepaid - Erc721CallerIsNotOwnerNorApproved - SupplyPositionDoesntBelongToTheLoan

--- contracts/test/Auction.t.sol
+++ contracts/test/Auction.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.17;
 
 import "./Commons/External.sol";
+import "./Commons/Internal.sol";
 
 contract TestAuction is External {
     // test simplest case of auction
@@ -51,4 +52,66 @@ contract TestAuction is External {
         vm.expectRevert(ERC721InvalidTokenId.selector);
         assertEq(kairos.ownerOf(1), address(0));
     }
+
+    function testLoanAlreadyRepaid() public {
+        BuyArgs[] memory args = new BuyArgs[](1);
+        uint256[] memory positionIds = new uint256[](1);
+        positionIds[0] = 1;
+        args[0] = BuyArgs({loanId: 1, to: signer2, positionIds: positionIds});
+        Loan memory loan = getLoan(); // price should be the same as lent amount
+        loan.startDate = block.timestamp - 1 weeks;
+        loan.endDate = block.timestamp - 2 days;
+        loan.payment.paid = 1 ether;
+        store(loan, 1);
+        mintPosition(signer, getProvision());
+        nft.mintOneTo(address(kairos));
+        vm.startPrank(signer);
+        vm.expectRevert(abi.encodeWithSelector(LoanAlreadyRepaid.selector, positionIds[0]));
+        kairos.buy(args);
+    }
+
+    function testErc721CallerIsNotOwnerNorApproved() public {
+        BuyArgs[] memory args = new BuyArgs[](1);
+        uint256[] memory positionIds = new uint256[](1);
+        positionIds[0] = 1;
+        args[0] = BuyArgs({loanId: 1, to: signer2, positionIds: positionIds});
+        Loan memory loan = getLoan(); // price should be the same as lent amount
+        loan.startDate = block.timestamp - 1 weeks;
+        loan.endDate = block.timestamp - 2 days;
+        store(loan, 1);
+        mintPosition(signer2, getProvision());
+        nft.mintOneTo(address(kairos));
+        vm.startPrank(signer);
+        vm.expectRevert(abi.encodeWithSelector(ERC721CallerIsNotOwnerNorApproved.selector));
+        kairos.buy(args);
+    }
+
+    function testSupplyPositionDoesntBelongToTheLoan() public {
+        BuyArgs[] memory args = new BuyArgs[](1);
+        uint256[] memory positionIds = new uint256[](1);
+        positionIds[0] = 1;
+        args[0] = BuyArgs({loanId: 1, to: signer2, positionIds: positionIds});
+        Loan memory loan = getLoan(); // price should be the same as lent amount
+        loan.startDate = block.timestamp - 1 weeks;
+        loan.endDate = block.timestamp - 2 days;
+        store(loan, 1);
+        Provision memory provision = getProvision();
+        provision.loanId = 2;
+        mintPosition(signer, provision);
+        nft.mintOneTo(address(kairos));
+        vm.startPrank(signer);
+        vm.expectRevert(abi.encodeWithSelector(SupplyPositionDoesntBelongToTheLoan.selector, args[0].positionIds[0], args[0].loanId));
+        kairos.buy(args);
+    }
+}
+
+contract TestAuctionInternal is Internal{
+
+
+
+    //Test SupplyPositionDoesntBelongToTheLoan
+
+
+
+
 }
