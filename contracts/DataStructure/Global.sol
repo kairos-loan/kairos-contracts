af23ba890bc79b0f209c5fc82994e5145c2a4387
npasquie <nicpas@live.fr>
2022-12-01 15:38:25 +0100

    🐛 fix eip712 offer encoding

--- contracts/DataStructure/Global.sol
+++ contracts/DataStructure/Global.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.17;
 
 import "./Errors.sol";
 
-bytes32 constant OFFER_TYPEHASH = keccak256("Offer(TODO)");
-
 bytes32 constant PROTOCOL_SP = keccak256("eth.kairos.protocol");
 bytes32 constant SUPPLY_SP = keccak256("eth.kairos.supply-position");
 
