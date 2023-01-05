d9c224c4559e2601865b1e47db730ad71ebd599a
npasquie <nicpas@live.fr>
2023-01-05 12:17:56 +0100

    💡 comment precision

--- script/DeployLocal.s.sol
+++ script/DeployLocal.s.sol
@@ -53,7 +53,7 @@ contract DeployLocal is Script, External {
         money.approve(address(kairos), 100 ether);
         nft.mintOne();
         nft.approve(address(kairos), 1);
-        frontNft.mintOneTo(address(kairos)); // kairos will be the owner of the nft for front testing
+        frontNft.mintOneTo(address(kairos)); // mint azuki #2 as liquidable for front auction testing
 
         vm.stopBroadcast();
 
