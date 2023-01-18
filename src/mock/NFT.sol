4c2467472366fd0ed0206b17e6331f038c7aff3a
npasquie <nicpas@live.fr>
2023-01-18 10:56:38 +0100

    ✨ mock nft made enumerable

--- src/mock/NFT.sol
+++ src/mock/NFT.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721, ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract NFT is ERC721 {
-    uint256 public totalSupply;
+contract NFT is ERC721Enumerable {
     string internal baseURI;
 
     /* solhint-disable-next-line no-empty-blocks */
@@ -12,16 +11,14 @@ contract NFT is ERC721 {
         baseURI = "ipfs://QmPMc4tcBsMqLRuCQtPmPe84bpSjrC3Ky7t3JWuHXYB4aS/"; // default: doodles metadata
     }
 
-    function mintOneTo(address to) public returns (uint256) {
-        totalSupply++;
-        _mint(to, totalSupply);
-        return totalSupply;
+    function mintOneTo(address to) public returns (uint256 createdId) {
+        createdId = totalSupply();
+        _mint(to, createdId);
     }
 
-    function mintOne() public returns (uint256) {
-        totalSupply++;
-        _safeMint(msg.sender, totalSupply);
-        return totalSupply;
+    function mintOne() public returns (uint256 createdId) {
+        createdId = totalSupply();
+        _safeMint(msg.sender, createdId);
     }
 
     function mintMultiple(uint256 amount) public {
