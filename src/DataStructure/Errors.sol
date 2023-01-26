d494ff8984077aea1418314d25be25d59e507623
npasquie <nicpas@live.fr>
2023-01-26 12:25:21 +0100

    🚀 edit env of api + selectors of errors

--- src/DataStructure/Errors.sol
+++ src/DataStructure/Errors.sol
@@ -5,13 +5,13 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {NFToken, Offer} from "./Objects.sol";
 
-error CollateralDoesntMatchSpecs(IERC721 sentCollateral, uint256 tokenId);
-error BadCollateral(Offer offer, NFToken providedNft);
-error OfferHasExpired(Offer offer, uint256 expirationDate);
-error RequestedAmountIsNull(Offer offer);
-error RequestedAmountTooHigh(uint256 requested, uint256 offered);
-error InconsistentAssetRequests(IERC20 firstRequested, IERC20 requested);
-error LoanAlreadyRepaid(uint256 loanId);
-error SupplyPositionDoesntBelongToTheLoan(uint256 positionId, uint256 loanId);
-error NotBorrowerOfTheLoan(uint256 loanId);
-error BorrowerAlreadyClaimed(uint256 loanId);
+error CollateralDoesntMatchSpecs(IERC721 sentCollateral, uint256 tokenId); // 0x9a295ec5
+error BadCollateral(Offer offer, NFToken providedNft); // 0xd8ef221b
+error OfferHasExpired(Offer offer, uint256 expirationDate); // 0x065a5c3b
+error RequestedAmountIsNull(Offer offer); // 0x8ea5f1d7
+error RequestedAmountTooHigh(uint256 requested, uint256 offered); // 0xf6d81a2c
+error InconsistentAssetRequests(IERC20 firstRequested, IERC20 requested); // 0x46aac2a9
+error LoanAlreadyRepaid(uint256 loanId); // 0xdae2c273
+error SupplyPositionDoesntBelongToTheLoan(uint256 positionId, uint256 loanId); // 0xf109be00
+error NotBorrowerOfTheLoan(uint256 loanId); // 0xc250ea5d
+error BorrowerAlreadyClaimed(uint256 loanId); // 0x16600edc
