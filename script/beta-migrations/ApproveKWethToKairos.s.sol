5a03100e4c7d20577c86ec2679e94fe89ca8df62
npasquie <nicpas@live.fr>
2023-01-27 14:33:22 +0100

    🚚 organise contract sol scripts

--- script/beta-migrations/ApproveKWethToKairos.s.sol
+++ script/beta-migrations/ApproveKWethToKairos.s.sol
@@ -3,14 +3,16 @@ pragma solidity 0.8.17;
 
 import {Script} from "forge-std/Script.sol";
 
-import {TestCurrency} from "../src/mock/TestCurrency.sol";
+import {IKairos} from "../../interface/IKairos.sol";
+import {TestCurrency} from "../../src/mock/TestCurrency.sol";
 
-contract MintKWEthToSupplier is Script {
+contract ApproveKWethToKairos is Script {
     function run() public {
         uint256 deployerKey = vm.envUint("DEPLOYER_KEY");
         TestCurrency betaFakeWeth = TestCurrency(0x659e527F2A0de2ABc954351a736Ec4157d90A379);
+        IKairos kairos = IKairos(0xCA3624F4B6872662014588C5D7833e959eefa5Eb);
 
         vm.startBroadcast(deployerKey);
-        betaFakeWeth.mint(100_000_000 ether);
+        betaFakeWeth.approve(address(kairos), 100_000_000 ether);
     }
 }
--- script/deploy/DeployNftAndMoney.s.sol
+++ script/deploy/DeployNftAndMoney.s.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.17;
 
 import {Script} from "forge-std/Script.sol";
 
-import {Money} from "../src/mock/Money.sol";
-import {NFT} from "../src/mock/NFT.sol";
+import {Money} from "../../src/mock/Money.sol";
+import {NFT} from "../../src/mock/NFT.sol";
 
 contract Deploy is Script {
     function run() public {
