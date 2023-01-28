ec33387d27fb868d0679d3ad197922082c506a83
npasquie <nicpas@live.fr>
2023-01-28 19:05:51 +0100

    💄 fix dashboard rows

--- script/beta-migrations/ApproveKWethToKairos.s.sol
+++ script/beta-migrations/ApproveKWethToKairos.s.sol
@@ -10,7 +10,7 @@ contract ApproveKWethToKairos is Script {
     function run() public {
         uint256 deployerKey = vm.envUint("DEPLOYER_KEY");
         TestCurrency betaFakeWeth = TestCurrency(0x659e527F2A0de2ABc954351a736Ec4157d90A379);
-        IKairos kairos = IKairos(0xCA3624F4B6872662014588C5D7833e959eefa5Eb);
+        IKairos kairos = IKairos(0x924ee2047cC11c3E293BFE95d4664986c6fc2D90);
 
         vm.startBroadcast(deployerKey);
         betaFakeWeth.approve(address(kairos), 100_000_000 ether);
