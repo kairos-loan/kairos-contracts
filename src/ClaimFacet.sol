f55a13a35e0f82e08d307800edd0b2008431d51b
Baptiste Marchand <baptiste.marchand4@protonmail.com>
2023-02-03 15:00:38 +0100

    contracts: add erc20 transfer revert on error (#155)

--- src/ClaimFacet.sol
+++ src/ClaimFacet.sol
@@ -84,7 +84,9 @@ contract ClaimFacet is IClaimFacet, SafeMint {
         Ray shareOfTotalLent = provision.amount.div(loan.lent);
         // todo #178 explore why precision improvement breaks tests
         sent = provision.amount + (loan.payment.paid - loan.lent).mul(shareOfTotalLent);
-        loan.assetLent.transfer(msg.sender, sent);
+        if (!loan.assetLent.transfer(msg.sender, sent)) {
+            revert ERC20TransferFailed(loan.assetLent, address(this), msg.sender);
+        }
     }
 
     /// @notice sends liquidation share due to `msg.sender` as a supplier
