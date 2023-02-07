3f85d1f33c733a53dce1d92ed4be2f7f3e5dcb00
Edouard Bougon <edouard@bougon.fr>
2023-02-07 10:54:45 +0100

    Contracts - Dispatch root command into contracts and chain-types contracts (#275)

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -5,7 +5,7 @@
 rm -rf /usr/app/packages/shared/src/generated/deployment.json
 
 # Start blockchain node
-anvil --silent --block-time 1 --timestamp $(date +%s) --allow-origin "*" --host 0.0.0.0 > out/anvil-logs.txt & PID_ANVIL=$!
+anvil --silent --block-time 1 --timestamp $(date +%s) --allow-origin "*" --host 0.0.0.0 > packages/contracts/out/anvil-logs.txt & PID_ANVIL=$!
 
 # Wait install state to be ready
 /usr/bin/wait
@@ -15,14 +15,21 @@ anvil --silent --block-time 1 --timestamp $(date +%s) --allow-origin "*" --host 
 export ENTR_INOTIFY_WORKAROUND=true;
 while sleep 1; do 
   find packages/contracts -type f -name "*.sol"  | entr -d -n sh -c '
+  
     echo "Start to build contract"
+    cd packages/contracts
     forge build --use=/usr/bin/solc --extra-output-files abi
+    cd ../..
 
     echo "Start to generate types"
-    yarn gen-typechain
+    cd packages/chain-types
+    yarn generate
+    cd ../..
 
     echo "Start to deploy contract"
-    yarn dot-env && forge script ./packages/contracts/script/DeployLocal.s.sol -f http://contracts:8545 --no-auto-detect --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    cd packages/contracts
+    forge script ./scripts/sol/DeployLocal.s.sol -f http://contracts:8545 --no-auto-detect --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    cd ../..
 
     echo "Finished. Watching for new change"
   '; 
