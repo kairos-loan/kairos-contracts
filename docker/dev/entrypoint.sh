69d4cd030e99863dd0f775b9bdc190707dc3d73c
Edouard Bougon <edouard@bougon.fr>
2023-02-08 14:24:51 +0100

    Contract - Generate deployment into .env instead of json format (#289)

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Remove previous info of last deploy
 # Used to sync between container
-rm -rf /usr/app/packages/shared/src/generated/deployment.json
+rm -rf /usr/app/packages/contracts/out/deployment.env
 
 # Start blockchain node
 anvil --silent --block-time 1 --timestamp $(date +%s) --allow-origin "*" --host 0.0.0.0 > packages/contracts/out/anvil-logs.txt & PID_ANVIL=$!
@@ -14,8 +14,7 @@ anvil --silent --block-time 1 --timestamp $(date +%s) --allow-origin "*" --host 
 # And then build and deploy the contract
 export ENTR_INOTIFY_WORKAROUND=true;
 while sleep 1; do 
-  find packages/contracts -type f -name "*.sol"  | entr -d -n sh -c '
-  
+  find packages/contracts -type f -name "*.sol"  | entr -d -n sh -c '  
     echo "Start to build contract"
     cd packages/contracts
     forge build --use=/usr/bin/solc --extra-output-files abi
