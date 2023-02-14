7e9ed5f1fc7c0aeb12ceb0fb88e31e3149707260
npasquie <nicpas@live.fr>
2023-02-14 17:42:44 +0100

    contract: add admin interface (#71)

--- src/AdminFacet.sol
+++ src/AdminFacet.sol
@@ -2,26 +2,15 @@
 pragma solidity 0.8.17;
 
 import {IOwnershipFacet} from "./interface/IOwnershipFacet.sol";
+import {IAdminFacet} from "./interface/IAdminFacet.sol";
+
 import {Ray} from "./DataStructure/Objects.sol";
 import {SupplyPosition, Protocol} from "./DataStructure/Storage.sol";
 import {protocolStorage} from "./DataStructure/Global.sol";
 import {CallerIsNotOwner} from "./DataStructure/Errors.sol";
 
 /// @notice admin-only setters for global protocol parameters
-contract AdminFacet {
-    /// @notice duration of future auctions has been updated
-    /// @param newAuctionDuration duration of liquidation for new loans
-    event NewAuctionDuration(uint256 indexed newAuctionDuration);
-
-    /// @notice initial price factor of future auctions has been updated
-    /// @param newAuctionPriceFactor factor of loan to value setting initial price of auctions
-    event NewAuctionPriceFactor(Ray indexed newAuctionPriceFactor);
-
-    /// @notice a new interest rate tranche has been created
-    /// @param tranche the interest rate of the new tranche, in multiplier per second
-    /// @param newTrancheId identifier of the new tranche
-    event NewTranche(Ray indexed tranche, uint256 indexed newTrancheId);
-
+contract AdminFacet is IAdminFacet {
     /// @notice restrict a method access to the protocol owner only
     modifier onlyOwner() {
         address admin = IOwnershipFacet(address(this)).owner();
---
+++ src/interface/IAdminFacet.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {Ray} from "../DataStructure/Objects.sol";
+
+interface IAdminFacet {
+    /// @notice duration of future auctions has been updated
+    /// @param newAuctionDuration duration of liquidation for new loans
+    event NewAuctionDuration(uint256 indexed newAuctionDuration);
+
+    /// @notice initial price factor of future auctions has been updated
+    /// @param newAuctionPriceFactor factor of loan to value setting initial price of auctions
+    event NewAuctionPriceFactor(Ray indexed newAuctionPriceFactor);
+
+    /// @notice a new interest rate tranche has been created
+    /// @param tranche the interest rate of the new tranche, in multiplier per second
+    /// @param newTrancheId identifier of the new tranche
+    event NewTranche(Ray indexed tranche, uint256 indexed newTrancheId);
+
+    function setAuctionDuration(uint256 newAuctionDuration) external;
+
+    function setAuctionPriceFactor(Ray newAuctionPriceFactor) external;
+
+    function createTranche(Ray newTranche) external returns (uint256 newTrancheId);
+}
