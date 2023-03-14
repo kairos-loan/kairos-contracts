36a1fcb352504feed02e7dc6cd2661975697b60d
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-03-14 14:37:12 -0700

    contract: fix pragmas of MockERC721 (audit)

--- test/DiamondERC721/MockERC721.sol
+++ test/DiamondERC721/MockERC721.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
 
 import {DiamondERC721} from "../../src/SupplyPositionLogic/DiamondERC721.sol";
 
