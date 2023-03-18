01c8e4e80c88b3b7a81275248693f3a045d873a7
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-03-18 01:47:53 -0700

    contract: some cleaning (audit)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -48,6 +48,7 @@ const config: HardhatUserConfig = {
     },
     local: {
       url: "http://127.0.0.1:8545",
+      // default test key of hardhat and anvil
       accounts: ["ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
     },
     hardhat: {
